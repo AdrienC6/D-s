b696302ab5c2cb65fcaf1ef7d8edb80eaa234467
AdrienC6 <72017587+AdrienC6@users.noreply.github.com>
2021-02-07 14:29:10 +0100

    rules & functional game OK

--- js/main.js
+++ js/main.js
@@ -1,56 +1,74 @@
-const rules = document.getElementById('rules');
+const de = document.querySelector('#de');
+const save = document.querySelector('#save');
+const again = document.querySelector('#again');
+const tour1 = document.querySelector('#tour1');
+const total1 = document.querySelector('#total1');
+const tour2 = document.querySelector('#tour2');
+const total2 = document.querySelector('#total2');
 
-rules.addEventListener('click', () => {
-    alert('Règles du jeu : Cliquez sur le dé pour le lancer. Le résultat du dé est ajouté à votre Tour Score. Vous pouvez relancer le dé pour augmenter votre score, ou sauvegarder vos points dans votre Total. Attention, si vous obtenez un 1, vous perdez vos points et c\'est à l\'autre joueur de lancer le dé. Le premier joueur à atteindre un Total de 100 remporte la partie.');
+const activePlayerTour = document.getElementById('active-player-tour'); // A qui le tour
+const player1 = { "name": "1", "tour": tour1.innerHTML, "total": total1.innerHTML };
+const player2 = { "name": "2", "tour": tour2.innerHTML, "total": total2.innerHTML };
+var activePlayer = player1;
+
+// console.log(player1);
+// console.log(player2);
+activePlayerTour.innerHTML = "C'est au tour du Joueur " + activePlayer.name;
+
+de.addEventListener('click', () => {
+    var randomNumber = Math.floor(Math.random() * (1 - 7) + 7);
+    de.style.border = "none";
+    console.log(randomNumber)
+    switch (randomNumber) {
+        case 1:
+            de.src = "../images/1.png";
+            de.style.border = "2px solid red";
+            activePlayer === player1 ? tour1.innerHTML = 0 : tour2.innerHTML = 0; // Score du tour passe à 0
+            activePlayer = activePlayer === player1 ? player2 : player1; // Si joueur = 1 alors devient 2, si 2 devient 1
+            activePlayerTour.innerHTML = "C'est au tour du Joueur " + activePlayer.name; // Annonce du joueur
+            randomNumber = 0;
+            break;
+        case 2:
+            de.src = "../images/2.png";
+            break;
+        case 3:
+            de.src = "../images/3.png";
+            break;
+        case 4:
+            de.src = "../images/4.png";
+            break;
+        case 5:
+            de.src = "../images/5.png";
+            break;
+        case 6:
+            de.src = "../images/6.png";
+            break;
+    }
+
+    activePlayer === player1 ? tour1.innerHTML = parseInt(tour1.innerHTML) + randomNumber : tour2.innerHTML = parseInt(tour2.innerHTML) + randomNumber;
+
+});
+
+save.addEventListener('click', () => {
+    if (activePlayer === player1) { // Si joueur 1, total = + tour 1
+        total1.innerHTML = parseInt(total1.innerHTML) + parseInt(tour1.innerHTML)
+    } else {
+        total2.innerHTML = parseInt(total2.innerHTML) + parseInt(tour2.innerHTML); // Sinon total = 2
+
+    }
+    activePlayer === player1 ? tour1.innerHTML = 0 : tour2.innerHTML = 0; // Score du tour passe à 0
+
+    activePlayer = activePlayer === player1 ? player2 : player1;
+
+
+    activePlayerTour.innerHTML = "C'est au tour du Joueur " + activePlayer.name;
+
+    if (parseInt(total1.innerHTML) >= 100) {
+        activePlayerTour.innerHTML = "Victoire du joueur " + player1.name;
+    }
+
+    if (parseInt(total2.innerHTML) >= 100) {
+        activePlayerTour.innerHTML = "Victoire du joueur " + player2.name;
+    }
 })
 
-// ------------------------------------------------
-
-const activePlayerTour = document.getElementById('active-player-tour');
-var activePlayer = "Joueur 1";
-
-activePlayerTour.innerHTML = "C'est au tour du " + activePlayer;
-
-
-const de = document.getElementById('de');
-
-
-function play() {
-    de.addEventListener('click', () => {
-        const randomNumber = Math.floor(Math.random() * (1 - 7) + 7)
-        console.log(randomNumber)
-
-        de.setAttribute('class', 'animate__animated animate__bounce');
-        setTimeout(() => {
-            de.removeAttribute('class', 'animate__animated animate__bounce');
-        }, 500);
-
-        switch (randomNumber) {
-
-            case 1:
-                de.src = "../images/1.png";
-                break;
-            case 2:
-                de.src = "../images/2.png";
-                break;
-            case 3:
-                de.src = "../images/3.png";
-                break;
-            case 4:
-                de.src = "../images/4.png";
-                break;
-            case 5:
-                de.src = "../images/5.png";
-                break;
-            case 6:
-                de.src = "../images/6.png";
-                break;
-        }
-
-
-
-    })
-
-}
-
-// -------------------------------------------------
